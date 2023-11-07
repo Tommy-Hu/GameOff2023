a904433cd2aee5177e92dba2f569bb7af8a04412
Popseratzy <jiuethan@gmail.com>
2023-11-06 23:38:58 -0800

    frog

---
+++ scripts/Frog.cs
@@ -0,0 +1,65 @@
+using Godot;
+using System;
+
+public partial class Frog : RigidBody2D
+{
+	public float jumpForceMin = 200;
+	public float jumpForceMax = 1000;
+	public float jumpinterval = 15;
+	public float curJumpForce = 200;
+	public bool jumpIncreaseing;
+	public bool jumpHeld;
+	public Sprite2D direction;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Area2D area2D = this.GetNode<Area2D>("GroundCheck");
+		direction = this.GetNode<FrogJumpDirector>("FrogJumpDirector");
+		curJumpForce = jumpForceMin;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+        
+	}
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+
+		if (jumpHeld)
+		{
+			if (curJumpForce <= jumpForceMin)
+			{
+				jumpIncreaseing = true;
+			}
+			if (curJumpForce >= jumpForceMax)
+            {
+				jumpIncreaseing = false;
+			}
+			curJumpForce = jumpIncreaseing ? (curJumpForce + jumpinterval) : (curJumpForce - jumpinterval);
+			
+			
+			GD.Print(curJumpForce);
+			GD.Print(jumpIncreaseing);
+
+		}
+
+		if (Input.IsActionPressed("FrogJump"))
+		{
+			
+			jumpHeld = true;
+		}
+		else if (Input.IsActionJustReleased("FrogJump"))
+        {
+			jumpHeld = false;
+			ApplyImpulse(new Vector2(curJumpForce * direction.Position.Normalized().X, curJumpForce * direction.Position.Normalized().Y), new Vector2(0, 0));
+			curJumpForce = jumpForceMin;
+		}
+	}
+
+
+}
---
+++ scripts/FrogJumpDirector.cs
@@ -0,0 +1,51 @@
+using Godot;
+using System;
+
+public partial class FrogJumpDirector : Sprite2D
+{
+	private float distance;
+	private float cycleTime = 2;
+	private double angle = 0;
+	private bool clockwise;
+	private bool canSpin = true;
+	public Frog frog;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		frog = GetParent<Frog>();
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (!frog.jumpHeld)
+		{
+
+			if (angle >= Math.PI)
+			{
+				clockwise = false;
+			}
+			else if (angle <= 0)
+			{
+				clockwise = true;
+			}
+
+			if (!clockwise)
+			{
+				angle = angle - 0.1;
+			}
+			else if (clockwise)
+			{
+				angle = angle + 0.1;
+			}
+
+			this.Rotation = -(float)angle + (float)Math.PI/2;
+
+
+			Position = new Vector2((float)Math.Cos(angle) * 100, (float)Math.Sin(angle) * -100);
+		}
+
+	}
+}
