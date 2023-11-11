12063977d6f23319bda876bad2ba895950b1f6de
Tommy Hu <tommy_20040909@outlook.com>
2023-11-10 19:01:58 -0800

    Added squishing of cells and particles. Added player movement. Added SelfDestroyingParticle.

---
+++ scripts/Cell.cs
@@ -0,0 +1,77 @@
+using Godot;
+using System;
+
+/// <summary>
+/// Represents a non-player cell.
+/// </summary>
+public partial class Cell : StaticBody2D
+{
+    public static readonly Color GOOD_COLOR = new Color(1, 1, 1, 1f);
+    public static readonly Color BAD_COLOR = new Color(1, .1f, .15f, 1f);
+
+    [Export]
+    public bool good;
+
+    private static RandomNumberGenerator rng = null;
+
+    private bool squishing = false;
+    private PlayerCell squishTo = null;
+    private Sprite2D sprite;
+    private ShaderMaterial material;
+    private float slurpProcess;
+
+    public override void _Ready()
+    {
+        if (rng == null)
+        {
+            rng = new RandomNumberGenerator();
+            rng.Seed = ~0x7531357UL ^ Time.GetTicksMsec();
+        }
+        base._Ready();
+        sprite = GetChild<Sprite2D>(1);
+        material = (sprite.Material.Duplicate() as ShaderMaterial);
+        sprite.Material = material;
+        material.SetShaderParameter("offset", new Vector2(rng.Randf(), rng.Randf()));
+        material.SetShaderParameter("tint", good ? GOOD_COLOR : BAD_COLOR);
+        material.SetShaderParameter("squish", 0.0f);
+        var particle = GetChild<SelfDestroyingParticle>(2);
+        ParticleProcessMaterial ppm = (particle.ProcessMaterial.Duplicate() as ParticleProcessMaterial);
+        particle.ProcessMaterial = ppm;
+        var colorRamp = ResourceLoader.Load<GradientTexture1D>($"res://scenes/prefabs/{(good ? "good" : "bad")}_cell_color_ramp.tres");
+        ppm.ColorRamp = colorRamp;
+    }
+
+    public void Slurp(PlayerCell into)
+    {
+        squishing = true;
+        squishTo = into;
+        var particle = GetChild<SelfDestroyingParticle>(2);
+        particle.StartSelfDestroy(2f);
+        particle.Emitting = true;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+        if (squishing)
+        {
+            GetChild<CollisionShape2D>(0).Disabled = true;
+            CollisionLayer = 0;
+            CollisionMask = 0;
+            if (slurpProcess >= 1.0f)
+            {
+                var particle = GetChild<SelfDestroyingParticle>(2);
+                particle.Detatch(GetParent());
+                QueueFree();
+                return;
+            }
+            material.SetShaderParameter("squish", slurpProcess);
+            GlobalPosition = GlobalPosition.Lerp(squishTo.GlobalPosition, slurpProcess * slurpProcess);
+            // compare the second column of the basis matrices to rotate
+            //GlobalRotation = Mathf.Lerp(GlobalRotation, GlobalRotation + GlobalTransform.Y.AngleTo(squishTo.GlobalTransform.Y), 1);
+            float angle = (squishTo.GlobalPosition - GlobalPosition).Angle() + Mathf.Pi * 0.5f;
+            GlobalRotation = Mathf.LerpAngle(GlobalRotation, angle, 1);
+            slurpProcess += (float)delta;
+        }
+    }
+}
