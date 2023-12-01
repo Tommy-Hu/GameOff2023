7f1dc19cabf315d4541f80aa3f11471cdaddf6d3
Edrick Belmiro <edrickbelmiro@gmail.com>
2023-12-01 13:06:34 -0800

    Test

--- scripts/electron_scripts/MainElectron.cs
+++ scripts/electron_scripts/MainElectron.cs
@@ -2,14 +2,13 @@ using Godot;
 
 public partial class MainElectron : Node2D
 {
-    PackedScene mobScene;
-    int x;
+    PackedScene mobScene;    
 
     public override void _Ready()
     {
         mobScene = ResourceLoader.Load<PackedScene>("res://scenes/prefabs/photon.tscn");
         // Create a new instance of the Mob scene.
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 6; i++)
         {
             // Create a new instance of the Mob scene.
             Photon mob = mobScene.Instantiate<Photon>();
@@ -27,7 +26,6 @@ public partial class MainElectron : Node2D
 
         //// Spawn the mob by adding it to the Main scene.
         //GetChild(2).AddChild(mob);
-        x++;
     }
 
     public void OnPhotonGracePeriodEnd()
