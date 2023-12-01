b500bdc191fc770f9b3ba4012718737d4076ea74
Edrick Belmiro <edrickbelmiro@gmail.com>
2023-12-01 10:27:17 -0800

    Polishing semi complete

--- scripts/electron_scripts/MainElectron.cs
+++ scripts/electron_scripts/MainElectron.cs
@@ -2,13 +2,14 @@ using Godot;
 
 public partial class MainElectron : Node2D
 {
-    PackedScene mobScene;    
+    PackedScene mobScene;
+    int x;
 
     public override void _Ready()
     {
         mobScene = ResourceLoader.Load<PackedScene>("res://scenes/prefabs/photon.tscn");
         // Create a new instance of the Mob scene.
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < 5; i++)
         {
             // Create a new instance of the Mob scene.
             Photon mob = mobScene.Instantiate<Photon>();
@@ -26,6 +27,7 @@ public partial class MainElectron : Node2D
 
         //// Spawn the mob by adding it to the Main scene.
         //GetChild(2).AddChild(mob);
+        x++;
     }
 
     public void OnPhotonGracePeriodEnd()
--- scripts/electron_scripts/PlayerElectron.cs
+++ scripts/electron_scripts/PlayerElectron.cs
@@ -6,72 +6,32 @@ public partial class PlayerElectron : SubAtomicCharge
 {
     int FORCE_ADJUSTMENT = 200;
     private AnimatedSprite2D daSprite;
-    private Node2D arrowRotator;
-    private Node2D movementArrowRotator;
-    private Area2D goal;
-    private GpuParticles2D sprintParticles;
     bool powered = false;
-    
+
 	List<SubAtomicCharge> electromagneticCharges = new() { };
 
     public override void _Ready()
     {
         base._Ready();
         Charge = -1;
-        
-        
-        goal = GetParent().GetChild<Area2D>(6);
-
         daSprite = GetChild<AnimatedSprite2D>(1);
-        arrowRotator = GetChild<Node2D>(3);
-        movementArrowRotator = GetChild<Node2D>(4);
-        sprintParticles = GetChild<GpuParticles2D>(5);
-
         GameManager.OnBeat += GameManager_OnBeat;
     }
 
     public override void _Process(double delta)
     {
         daSprite.Play();
-        RotateArrow();
-    }
-
-    private void RotateArrow()
-    {
-        Vector2 vectorToGoal = goal.GlobalPosition - GlobalPosition;
-        if (vectorToGoal.X > 0)
-        {
-            arrowRotator.GlobalRotation = Mathf.Atan(vectorToGoal.Y / vectorToGoal.X);
-        }
-        else
-        {
-            arrowRotator.GlobalRotation = Mathf.Pi + Mathf.Atan(vectorToGoal.Y / vectorToGoal.X);
-        }
-        arrowRotator.GlobalRotation += Mathf.Pi / 2;
-    }
-
-    private void RotateMovementArrow(Vector2 v)
-    {
-        if (v.X >= 0)
-        {
-            movementArrowRotator.GlobalRotation = Mathf.Atan(v.Y / v.X);
-        }
-        else 
-        {
-            movementArrowRotator.GlobalRotation = Mathf.Pi + Mathf.Atan(v.Y / v.X);
-        }
-        movementArrowRotator.GlobalRotation += Mathf.Pi / 2;
     }
 
     private void GameManager_OnBeat(BeatType beat)
     {
         if (beat.HasFlag(BeatType.PhraseBeat))
         {
-            ApplyCentralImpulse(CalculateElectromagneticPull(8) / 16);
+            ApplyCentralImpulse(CalculateElectromagneticPull(4) / 16);
         }
         if (beat.HasFlag(BeatType.MainBeat))
         {
-            //ApplyCentralImpulse(CalculateElectromagneticPull(1) / 16);
+            ApplyCentralImpulse(CalculateElectromagneticPull(1) / 16);
         }
         if (beat.HasFlag(BeatType.QuarterBeat))
         {
@@ -94,28 +54,11 @@ public partial class PlayerElectron : SubAtomicCharge
         if (Input.IsKeyPressed(Key.S)) movement += Vector2.Down;
         if (Input.IsKeyPressed(Key.D)) movement += Vector2.Right;
         if (movement != Vector2.Zero) movement = movement.Normalized();
-        if (movement == Vector2.Zero)
-            movementArrowRotator.Visible = false;
-        else
-        {
-            RotateMovementArrow(movement);
-            movementArrowRotator.Visible = true;
-        }
         if (powered)
-        {
-            ApplyForce(movement * FORCE_ADJUSTMENT / 20);
-            sprintParticles.Emitting = true;
-            movementArrowRotator.GetChild<Node2D>(0).Scale = new Vector2(0.5f, 0.3f);
-            if (LinearVelocity.LengthSquared() > 250_000)
-            {
-                LinearVelocity *= 250_000 / LinearVelocity.LengthSquared();
-            }
-        }
+            ApplyForce(movement * FORCE_ADJUSTMENT / 25);
         else
         {
             ApplyForce(movement * FORCE_ADJUSTMENT / 25);
-            sprintParticles.Emitting = false;
-            movementArrowRotator.GetChild<Node2D>(0).Scale = new Vector2(0.5f, 0.2f);
             if (LinearVelocity.LengthSquared() > 90_000)
             {
                 LinearVelocity *= 90_000 / LinearVelocity.LengthSquared();
@@ -149,33 +92,12 @@ public partial class PlayerElectron : SubAtomicCharge
 
     public void Hit()
     {
-        GameManager.PlayRandomSFX("cell_bullet_splat.wav", "cell_bullet_splat1.wav");
-        GetChild(1).QueueFree();
-        GetChild(3).QueueFree();
-        GetChild(4).QueueFree();
-        GameManager.PlayLevelFade("electron", "Electron");
+        QueueFree();
+        GD.Print("You lose");
     }
 
-    public void OnPowerUpAreaEntered(Node2D body)
+    public void OnPowerUpAreaEntered()
     {
-        if (body == this)
-        {
-            powered = !powered;
-            if (powered)
-                GameManager.PlaySFX("power_up.wav");
-            else GameManager.PlaySFX("kill_bad_cell2.wav");
-        }
-    }
-
-    public void OnGoalAreaEntered(Node2D body)
-    {
-        if (body == this)
-        {
-            GameManager.PlayRandomSFX("kill_bad_cell1.wav");
-            GetParent<Node2D>().QueueFree();
-            GD.Print("You win");
-            GameManager.PlayLevelFade("cells", "Cells");
-        }
-        
+        powered = true;
     }
 }
